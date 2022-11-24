8fdc67a4fceeb8d1558b4f0d22e651d10ee7699a
AnjaliiBhardwaj <anjalibhardwaj823@gmail.com>
2022-11-24 23:28:35 +0530

    reverse

--- Arrays/ReverseanArray.cpp
+++ Arrays/ReverseanArray.cpp
@@ -1,13 +1,28 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int size;
+    int size,j;
     cin>>size;
     int arr[size];
 
     for(int i=0;i<size;i++){
         cin>>arr[i];
     }
-    
+    for (int i=0;i<size;i++){
+        int start=0;
+        int end=size-1;
+
+        while (start<=end){
+            for(i=0;j<size;j--){
+                int temp= arr[start];
+                arr[start]=arr[end];
+                arr[end]= temp; 
+            }
+        }
+
+    }
+    for (int i=0; i<size;i++){
+        cout<< arr[i] << " ";
+    }
 
 }
