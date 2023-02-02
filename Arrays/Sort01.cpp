c5c543e3f868437fc9688609e3dd4821398e83fa
AnjaliiBhardwaj <anjalibhardwaj823@gmail.com>
2023-02-02 23:59:31 +0530

    Commit

---
+++ Arrays/Sort01.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+void printArray(int arr[],int n){ 
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<< " ";
+    }
+    cout<<endl;
+}
+void sortOne (int arr[], int n){
+    int left=0, right = n-1;
+    while(left< right ){
+        while (arr[left]==0 && left<right){
+            left++;
+        }
+        while (arr[right]==1 && left<right){
+            right--;
+        }
+        if(left<right){
+        swap(arr[left],arr[right]);
+        left++;
+        right--;
+        }
+    }
+}
+int main(){
+    int arr[8]= {1,1,0,0,0,1,0};
+    sortOne(arr,0);
+    printArray(arr, 8);
+    return 0;
+}
