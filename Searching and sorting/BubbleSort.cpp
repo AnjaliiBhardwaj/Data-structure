e92c7773f5b0d2250c344ab01321ab09097a0040
AnjaliiBhardwaj <anjalibhardwaj823@gmail.com>
2023-05-10 23:40:58 +0530

    bubble

--- Searching and sorting/BubbleSort.cpp
+++ Searching and sorting/BubbleSort.cpp
@@ -10,7 +10,7 @@ int main(){
         cin>>arr[i];
     }
     for(int i=0;i<n;i++){
-        for(int j=0;j<n-1;j++){
+        for(int j=0;j<n-i;j++){
             if(arr[j]>arr[j+1]){
             max=arr[j];
             arr[j]=arr[j+1];
