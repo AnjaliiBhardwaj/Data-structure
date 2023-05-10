2fb3705a57446b6402fe78d348bcbb564b37188c
AnjaliiBhardwaj <anjalibhardwaj823@gmail.com>
2023-05-10 23:23:04 +0530

    bubble

---
+++ Searching and sorting/BubbleSort.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    int max=0;
+    cin>>n;
+    int arr[n];
+
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-1;j++){
+            if(arr[j]>arr[j+1]){
+            max=arr[j];
+            arr[j]=arr[j+1];
+            arr[j+1]= max;
+        }
+        }
+        
+    }
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
