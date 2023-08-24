ddf2ec9bbb7295c7fea851d32b23b617f7ad302b
AnjaliiBhardwaj <anjalibhardwaj823@gmail.com>
2023-08-24 23:47:13 +0530

    Commit

---
+++ Arrays/KshiftRotate.cpp
@@ -0,0 +1,18 @@
+/*
+Input: nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+*/
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        
+    }
+};
