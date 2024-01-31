1d493f06acaf5397c3998382b0c5996f55380cb8
AnjaliiBhardwaj <anjalibhardwaj823@gmail.com>
2024-02-01 00:13:56 +0530

    Input and output levelwise

--- Binary Tree/InputIteratively.java
+++ Binary Tree/InputIteratively.java
@@ -3,30 +3,58 @@ class InputIteratively{
     public static BinaryTreeNode<Integer> takeInput(){
         Scanner sc=new Scanner(System.in);
         int rootdata=sc.nextInt();
-        if(root.data==-1){
+        if(rootdata==-1){
             return null;
         }
-        Queue <BinaryTreeNode<Integer>> pendingNode=new LinkedList<>(rootdata);
         BinaryTreeNode<Integer> root=new BinaryTreeNode<>(rootdata);
+        Queue <BinaryTreeNode<Integer>> pendingNode=new LinkedList<>();
+        pendingNode.add(root);
         while(!pendingNode.isEmpty()){
-            BinaryTreeNode <Integer> frontNode=pendingNode.poll();
-            int leftChild=sc.nextInt();
-            if(leftChild!=-1){
-                BinaryTreeNode<Integer> child=new BinaryTreeNode<>(leftChild);
-                frontNode.left.add(child);
-                pendingNode.add(child);
+            BinaryTreeNode<Integer> frontNode=pendingNode.poll();
+            int leftNodeData=sc.nextInt();
+            if(leftNodeData!=-1){
+                BinaryTreeNode<Integer> leftNode = new BinaryTreeNode<>(leftNodeData);
+                frontNode.left=leftNode;
+                pendingNode.add(leftNode);
             }
-            int rightChild=sc.nextInt();
-            if(rightChild!=-1){
-                BinaryTreeNode<Integer> child=new BinaryTreeNode<>(rightChild);
-                frontNode.right.add(child);
-                pending.add(child);
+            int rightNodeData=sc.nextInt();
+            if(rightNodeData!=-1){
+                BinaryTreeNode<Integer> rightNode = new BinaryTreeNode<>(rightNodeData);
+                frontNode.right=rightNode;
+                pendingNode.add(rightNode);
             }
-
         }
         return root;
     }
     public static void print(BinaryTreeNode<Integer> root){
+        if(root==null){
+            return;
+        }
+        Queue <BinaryTreeNode<Integer>> pendingNode=new LinkedList<>();
+        pendingNode.add(root);
+        while(!pendingNode.isEmpty()){
+            BinaryTreeNode<Integer> front=pendingNode.poll();
+            if(front!=null){
+                System.out.print(front.data);
+                if(front.left==null){
+                System.out.print("L:-1");
+            }
+                else{
+                System.out.print("L:"+front.left.data);
+            }
+            if(front.right==null){
+                System.out.print("R:-1");
+            }
+            else{
+                System.out.print("R:"+front.right.data);
+            }
+            System.out.println();
+            pendingNode.add(front.left);
+            pendingNode.add(front.right);
+            }
+            
+        }
+
 
     }
     public static void main(String[] args){
