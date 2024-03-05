0570c7b5f0d53e6e2cf975a4ddc0cb9cf3c77c55
AnjaliiBhardwaj <anjalibhardwaj823@gmail.com>
2024-03-05 23:42:04 +0530

    15 leetcode 3sum

---
+++ Arrays/ThreeSum.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class ThreeSum{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            
+        }
+    }
+}
