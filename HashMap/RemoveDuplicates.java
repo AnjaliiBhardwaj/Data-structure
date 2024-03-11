a25df0c13000bdba37ac4da594b253c718c1a076
AnjaliiBhardwaj <anjalibhardwaj823@gmail.com>
2024-03-11 12:26:51 +0530

    Remove duplicate in arr

--- HashMap/RemoveDuplicates.java
+++ HashMap/RemoveDuplicates.java
@@ -1,5 +1,19 @@
 import java.util.*;
 class RemoveDuplicates{
+    public static ArrayList<Integer> checkDuplicate(int arr[]){
+        //O(n) space
+        //rather than creating an extra arralist of boolean of n+1 size just create hashmap which will track if that ele is present or not
+        ArrayList<Integer> ans=new ArrayList<>();
+        HashMap <Integer,Boolean> map= new HashMap<>();
+        for(int i=0;i<arr.length;i++){
+            if(map.containsKey(arr[i])){
+                continue;
+            }
+            ans.add(arr[i]);
+            map.put(arr[i],true);
+        }
+        return ans;
+    }
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
@@ -7,13 +21,9 @@ class RemoveDuplicates{
         for(int i=0;i<n;i++){
             arr[i]=sc.nextInt();
         }
-        HashMap <Integer,Integer> map= new HashMap<>();
-        for(Integer i:arr){
-            map.put(i);
+        ArrayList <Integer> output=checkDuplicate(arr);
+        for(int i=0;i<output.size();i++){
+            System.out.print(output.get(i)+" ");
         }
-        for(Integer i:map){
-            System.out.print(map.get(i)+" ");
-        }
-
     }
 }
