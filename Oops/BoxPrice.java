567ff043571c115238740fabe00940fabe0d4a91
AnjaliiBhardwaj <anjalibhardwaj823@gmail.com>
2024-04-27 23:20:47 +0530

    inheritance

---
+++ Oops/BoxPrice.java
@@ -0,0 +1,12 @@
+public class BoxPrice extends BoxWeight{
+    double cost ;
+    BoxPrice(){
+        super();
+        this.cost=-1;
+    }
+    BoxPrice(BoxPrice other){
+        super(other);
+        this.cost=other.cost;
+    }
+    public 
+}
