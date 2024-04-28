332d159fbdf21695a5a118127cf73b2c0e1c84f2
AnjaliiBhardwaj <anjalibhardwaj823@gmail.com>
2024-04-28 17:08:05 +0530

    Overriding

--- Oops/Polymorphism/Circle.java
+++ Oops/Polymorphism/Circle.java
@@ -1,4 +1,8 @@
 class Circle extends Shapes{
+
+    //this will run when obj of circle is created 
+    //hence it is overriding the parent method
+    @Override // annotation ,usually to check for if it is overriden or not
     void area(){
         System.out.println("Area is pie*r*r");
     }
