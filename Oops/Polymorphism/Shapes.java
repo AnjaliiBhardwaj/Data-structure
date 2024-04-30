8e68b594107ddde5a242868bacd690b9dc98e278
AnjaliiBhardwaj <anjalibhardwaj823@gmail.com>
2024-04-30 11:48:12 +0530

    final keyword

--- Oops/Polymorphism/Shapes.java
+++ Oops/Polymorphism/Shapes.java
@@ -3,4 +3,10 @@ class Shapes{
     void area(){
         System.out.println("Im a shape");
     }
+    //Early binding:
+    //now overriding can not be achieved
+    //even inheritance can be prevented
+    // final void area(){
+    //     System.out.println("I am in shape");
+    // }
 }
