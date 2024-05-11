15662b8057d11cf4f15821ada5f102910a0d7c72
AnjaliiBhardwaj <anjalibhardwaj823@gmail.com>
2024-05-11 22:37:18 +0530

    changes done

--- Oops/Main1.java
+++ Oops/Main1.java
@@ -1,19 +1,34 @@
 
-class Student{
-    private int rollNum;
-    String name;
+// class Student{
+//     private int rollNum;
+//     String name;
 
-    public Student(int rollNum,String name){
-        this.rollNum=rollNum;
+//     public Student(int rollNum,String name){
+//         this.rollNum=rollNum;
+//         this.name=name;
+//     }
+//     public int getRollNum(){
+//         return rollNum;
+//     }
+//     }
+// class Main1{
+//     public static void main(String[] args){
+//         Student s1=new Student(12,"Anjali");
+//         System.out.println(s1.name);
+//     }
+// }
+//-----Inheritance-------------
+class Animal{
+    String name;
+    public Animal(String name){
         this.name=name;
     }
-    public int getRollNum(){
-        return rollNum;
+    public void eat(){
+        System.out.println(name+" "+"is eating");
     }
-    }
+    public void 
+}
 class Main1{
     public static void main(String[] args){
-        Student s1=new Student(12,"Anjali");
-        System.out.println(s1.name);
     }
 }
