efd4303b2e1cfd6d414e716086976b773fc57e3d
AnjaliiBhardwaj <anjalibhardwaj823@gmail.com>
2024-06-17 19:36:47 +0530

    Bubble Sort

---
+++ Searching and sorting/BubbleSort.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class BubbleSort{
+    public static void helper(int n,int arr[]){
+        for(int i=0;i<n-1;i++){
+            //do not compare with already sorted ele
+            for(int j=0;j<n-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+
+        helper(n,arr);
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
